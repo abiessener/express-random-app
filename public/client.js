b4e0617a133bd3ed9113020b9be3381060d32002
Adam Biessener <abiessener@gmail.com>
2017-08-01 16:46:55 -0500

    Added a fancy button and whatnot to index.html

--- public/client.js
+++ public/client.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
 
+function displayResult(){
+    var newRand = randomNumber(0,100);
+    $('#resultsDiv').html("<p style='font-size: 2em; margin: 2px; text-align: center'>" + newRand + "</p>");
+    $('#resultsDiv').fadeIn();
+}
+
+$(document).ready(function(){
+    $('#newRandomButton').on('click', function(){
+        $('#resultsDiv').fadeOut(displayResult);
+    });
 });
